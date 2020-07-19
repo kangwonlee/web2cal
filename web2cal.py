21e60cd8073d5f4bc90ae1ea66f4f8a8b8cf51ee
KangWon LEE <kangwon.lee@kpu.ac.kr>
2020-07-20 07:55:13 +0900

    find hrefs
    # https://stackoverflow.com/questions/33817325

--- web2cal.py
+++ web2cal.py
@@ -2,6 +2,7 @@ import functools
 import sys
 import urllib.parse as up
 
+import lxml.html
 import pandas as pd
 import requests
 
@@ -9,6 +10,10 @@ import requests
 def main(argv=sys.argv):
     r = requests.get(get_avs_url())
     assert r.ok, r
+
+    # https://stackoverflow.com/questions/33817325
+    webpage = lxml.html.fromstring(r.content)
+    print(webpage.xpath('//a/@href'))
 
 
 @functools.lru_cache()
