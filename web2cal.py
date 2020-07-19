379e7f4e631d83b46abc010a15949382a4f305af
KangWon LEE <kangwon.lee@kpu.ac.kr>
2020-07-20 08:11:04 +0900

    refactored get_keynote_url()

--- web2cal.py
+++ web2cal.py
@@ -9,13 +9,21 @@ import requests
 
 
 def main(argv=sys.argv):
+    url = get_keynote_url()
+    print(url)
+
+
+def get_keynote_url():
     r = requests.get(get_avs_url())
 
     assert r.ok, r
 
     # https://stackoverflow.com/questions/33817325
     webpage = lxml.html.fromstring(r.content)
-    pprint.pprint(webpage.xpath('//a/@href'))
+    url = tuple(
+            filter(lambda link: 'a2zinc' in up.urlparse(link).netloc, webpage.xpath('//a/@href'))
+        )[0]
+    return url
 
 
 @functools.lru_cache()
